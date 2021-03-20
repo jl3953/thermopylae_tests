ca782249a55a4d5d6008d12060bca8005decb5cf
Jennifer Lam <jl87@princeton.edu>
2021-03-20 17:01:27 -0400

    syntax error

--- src/run_single_data_point.py
+++ src/run_single_data_point.py
@@ -206,7 +206,7 @@ def run_kv_workload(client_nodes, server_nodes, concurrency, keyspace, warm_up_d
     data_csv = os.path.join(constants.SCRATCH_DIR, data_csv_leaf)
     populate_crdb_data.populate(data_csv, keyspace)
     nfs_location = "data/{0}".format(data_csv_leaf)
-    upload_cmd = "{0} nodelocal upload {1} {2} --host={3}".format(
+    upload_cmd = "{0} nodelocal upload {1} {2} --host={3} --insecure".format(
         EXE, data_csv, nfs_location, a_server_node["ip"])
     system_utils.call(upload_cmd)
     import_cmd = "echo \"IMPORT INTO kv (k, v) CSV DATA('nodelocal://1/{1}');\" | " \
