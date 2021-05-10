6fbda70b280af2c7541a0d886df7489a562673ad
Jennifer Lam <jl87@princeton.edu>
2021-05-10 08:36:10 -0600

    implement suffix for graph generator

--- src/graph_tp_skew.py
+++ src/graph_tp_skew.py
@@ -12,6 +12,7 @@ def main():
     parser.add_argument("--db_file", type=str, required=True, help="trials.db")
     parser.add_argument("--csv_dir", type=str, required=True)
     parser.add_argument("--graph_dir", type=str, required=True)
+    parser.add_argument("--suffix", type=str, default="", help="add suffix to all generated files")
     args = parser.parse_args()
 
     # Query data
@@ -21,7 +22,8 @@ def main():
     c = db.c
     for row in c.execute("SELECT ops_per_sec_cum, p50_ms, p99_ms, skews "
                          "FROM trials_table "
-                         "WHERE server_nodes=3"):
+                         "WHERE server_nodes=4 "
+                         "AND n_keys_per_statement=1 "):
         data.append({
             "ops/sec(cum)": row[0],
             "p50(ms)": row[1],
@@ -33,13 +35,13 @@ def main():
     # sort and write out data
     data = sorted(data, key=lambda point: point["skews"])
     csv_file = csv_utils.write_out_data(data,
-                                        os.path.join(args.csv_dir, "dat.csv"))
+                                        os.path.join(args.csv_dir, "dat_{}.csv".format(args.suffix))
 
     # graph data
     plot_utils.gnuplot("src/plot.gp", csv_file,
-                       os.path.join(args.graph_dir, "p50_v_skew.png"),
-                       os.path.join(args.graph_dir, "tp_v_skew.png"),
-                       os.path.join(args.graph_dir, "p99_v_skew.png"))
+                       os.path.join(args.graph_dir, "p50_v_skew_{}.png".format(args.suffix)),
+                       os.path.join(args.graph_dir, "tp_v_skew_{}.png".format(args.suffix)),
+                       os.path.join(args.graph_dir, "p99_v_skew_{}.png".format(args.suffix))
 
     return 0
 
